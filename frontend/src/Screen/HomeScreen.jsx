047400e8d9552b2e4e140296eb65bd026e04bbd7
utkarsh032 <utkarshraj525@gmail.com>
2024-01-30 09:27:11 +0530

    fetchProducts from backend

--- frontend/src/Screen/HomeScreen.jsx
+++ frontend/src/Screen/HomeScreen.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('http://localhost:5000/api/products');
+      setProducts(data);
+      console.log(data)
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className='flex flex-wrap gap-6'>
       {products.map((product) => (
--- frontend/src/Screen/ProductScreen.jsx
+++ frontend/src/Screen/ProductScreen.jsx
@@ -1,11 +1,20 @@
 import { Link, useParams } from "react-router-dom"
-import products from '../products'
 import Rating from "../components/Rating"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState([])
   const { id: productId } = useParams()
-  const product = products.find((p) => p._id === productId)
-  console.log(product)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`http://localhost:5000/api/products/${productId}`)
+      setProduct(data);
+    }
+    fetchProduct()
+  }, [productId])
+
 
   return (
     <div className="h-screen">
